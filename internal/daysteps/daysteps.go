9b4b0f206a910f29bfabaa25e262474055339fd3
Vladlen <vector_rylet@mail.ru>
2025-10-31 20:07:28 +0300

    7

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -65,6 +65,7 @@ func DayActionInfo(data string, weight, height float64) string {
 	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
 	if err != nil {
 		log.Printf("Ошибка расчета калорий: %v", err)
+		return ""
 	}
 
 	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distanceKm, calories)
